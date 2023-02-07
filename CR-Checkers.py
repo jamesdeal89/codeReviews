7f3fb55ab5436985be3335ea4b7e6c5649637e1e
James Deal <j891319@protonmail.com>
2023-02-07 13:44:16 +0800

    instructions for CR 2

---
+++ CR-Checkers.py
@@ -0,0 +1,24 @@
+"""
+- Using a 2D list
+- create a representation of a checkers board's starting state (just the player positions)
+- N.B. You do not have to make any game functionality
+- Just a 2D list containing the starting state.
+- pass in the square board's size as a parameter
+- the minimum board size is 8, each player should have three rows of checkers
+- checkers should alternate
+- one player has red checkers (R)
+- one player has white checkers (W)
+
+Additionally please make a function to print out the checkerboard neatly (you may use whatever suitable symbols you like)
+
+The board of size 8 should look like this (just a representation) (_ represents an empty space, R represents a red checker, W represents a white Checker, spaces are not required)
+
+_ R _ R _ R _ R
+R _ R _ R _ R _
+_ R _ R _ R _ R
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ W _ W _ W _ W
+W _ W _ W _ W _
+_ W _ W _ W _ W
+"""
